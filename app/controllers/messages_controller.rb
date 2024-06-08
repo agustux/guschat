fd8ad42bd4aeeb3b77b24ec5b961b5dccf93b8c1
gus <gus@xubuntu>
2024-06-08 17:13:18 -0500

    filter by date, with links to today and prev_day

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,11 @@
 class MessagesController < ApplicationController
   http_basic_authenticate_with name: "gus", password: "gus123"
+  helper_method :prev_day
 
   def index
-    @messages = Message.all
+    logger.info "date: #{index_date}"
+    @messages = Message
+      .where(created_at: index_date.beginning_of_day..index_date.end_of_day)
   end
 
   def new
@@ -20,6 +23,24 @@ class MessagesController < ApplicationController
   end
 
   private
+
+    def prev_day
+      index_date - 1.day
+    end
+
+    def index_date
+      begin
+        index_params = params.permit(:date)
+        if index_params[:date]
+          Date.parse index_params[:date]
+        else
+          Date.today
+        end
+      rescue Date::Error => e
+        raise "invalid date"
+      end
+    end
+
     def message_params
       params.require(:message).permit(:body)
     end
